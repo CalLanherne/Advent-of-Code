c4fdb2da2b2f9224b2b2a54d5efa408205103bf0
Callum Lanherne <48178852+CalLanherne@users.noreply.github.com>
2023-12-04 22:12:23 +0000

    Completed day 2 part 2

--- Data/Day2Service.cs
+++ Data/Day2Service.cs
@@ -1,55 +1,34 @@
 using Microsoft.Extensions.FileSystemGlobbing.Internal;
 using System;
 using System.Drawing;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 namespace Advent_of_Code.Data
 {
     public class Day2Service
     {
-        public int SolveDay2(string input, int part) =>
+        public int SolveDay3(string input, int part) =>
             part switch
             {
-                1 => SolveDay2Part1(input, 12, 13, 14),
-                2 => SolveDay2Part2(input),
+                1 => SolveDay3Part1(input),
+                2 => SolveDay3Part2(input),
                 _ => throw new ArgumentOutOfRangeException(nameof(part), $"Unsupported part number {part}"),
             };
 
-        private int SolveDay2Part1(string input, int redNumber, int greenNumber, int blueNumber )
+        private int SolveDay3Part1(string input)
         {
             string[] games = input.Split("\n");
             var answer = 0;
-            foreach (string game in games)
-            {
-                if (MaxValueForColour(game, "green") <= greenNumber && MaxValueForColour(game, "blue") <= blueNumber && MaxValueForColour(game, "red") <= redNumber)
-                {
-                    var pattern = @"Game (\d+)";
-                    var match = Regex.Match(game, pattern);
-                    answer += Convert.ToInt32(match.Groups[1].Value);
-                }
-            }
+            
             return answer;
         }
 
-        private int MaxValueForColour(string game, string colour)
+        private int SolveDay3Part2(string input)
         {
-            var pattern = string.Format(@"(\d+)(?: {0})", colour);
-            var matches = Regex.Matches(game, pattern);
-            List<int> values = new List<int>();
-            values.Add(0);
-            foreach (Match match in matches)
-            {
-                values.Add(Convert.ToInt32(match.Groups[1].Value));
-            }
-            return values.Max();
-        }
-        private int SolveDay2Part2(string input)
-        {
-            var answer = 0;
+            string[] games = input.Split("\n");
+            
             return answer;
         }
-
-
-
     }
 }
